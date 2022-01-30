668ea82b83fa9fd00ea3f2f7a2697e65ab3e06d1
ANUJ BISHT <78249280+bishtanuj@users.noreply.github.com>
2022-01-30 08:52:10 +0530

    Create question_6.py

---
+++ QUESTIONS/question_6.py
@@ -0,0 +1,13 @@
+""" 
+Write a Python program which accepts a sequence of comma-separated numbers from user and generate a list and a tuple with those numbers. Go to the editor
+Sample data : 3, 5, 7, 23
+Output :
+List : ['3', ' 5', ' 7', ' 23']
+Tuple : ('3', ' 5', ' 7', ' 23')
+"""
+
+values = input("Input some comma separated numbers: ")
+list = values.split(",")
+tuple = tuple(list)
+print('List : ',list)
+print('tuple : ',tuple)
