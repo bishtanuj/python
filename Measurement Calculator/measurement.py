f7f1ded51d7b0df63511d8e67513813cec264f8b
ANUJ BISHT <78249280+bishtanuj@users.noreply.github.com>
2022-01-31 08:18:35 +0530

    Create measurement.py

---
+++ Measurement Calculator/measurement.py
@@ -0,0 +1,243 @@
+# Program to compute measurements
+
+from math import pi
+
+
+# Function to compute perimeter of circle
+def perimeterOfCircle():
+    radius = float(input("Enter the radius of circle: "))
+
+    perimeter = 2 * pi * radius
+    print("Perimeter of Circle: ", perimeter)
+
+
+# Function to compute perimeter of Rectangle
+def perimeterOfRectangle():
+    length = float(input("Enter the length of rectangle: "))
+    breadth = float(input("Enter the breadth of rectangle: "))
+
+    perimeter = 2 * length + breadth
+    print("Perimeter of Rectangle:", perimeter)
+
+
+# Function to compute perimeter of Rhombus
+def perimeterOfRhombus():
+    side = float(input("Enter the length of one side of Rhombus: "))
+
+    perimeter = 4 * side
+    print("Perimeter of Rhombus: ", perimeter)
+
+
+# Function to compute perimeter of Square
+def perimeterOfSquare():
+    side = float(input("Enter the length of one side of Square: "))
+
+    perimeter = 4 * side
+    print("Perimeter of Square:", perimeter)
+
+
+# Function to compute perimeter of Triangle
+def perimeterOfTriangle():
+    side_1 = float(input("Enter the length of side I: "))
+    side_2 = float(input("Enter the length of side II: "))
+    side_3 = float(input("Enter the length of side III: "))
+
+    perimeter = side_1 + side_2 + side_3
+    print("Perimeter of Triangle: ", perimeter)
+
+
+# Function to compute the area of Circle
+def areaOfCircle():
+    radius = float(input("Enter the radius of circle: "))
+
+    area = pi * radius ** 2
+    print("Area of Circle: ", area)
+
+
+# Function to compute the area of Rectangle
+def areaOfRectangle():
+    length = float(input("Enter the length of Rectangle: "))
+    breadth = float(input("Enter the breadth of Rectangle: "))
+
+    area = length * breadth
+    print("Area of Rectangle: ", area)
+
+
+# Function to compute the area of Rhombus
+def areaOfRhombus():
+    diagonalone = float(input("Enter the length of the diagonal I: "))
+    diagonaltwo = float(input("Enter the length of the diagonal II: "))
+
+    area = (diagonalone * diagonaltwo) / 2
+    print("Area of Rhombus: ", area)
+
+
+# Function to compute the area of Square
+def areaOfSquare():
+    side = float(input("Enter the length of one side of Square: "))
+
+    area = side * side
+    print("Area of Square: ", area)
+
+
+# Function to compute the area of Triangle
+def areaOfTriangle():
+    height = float(input("Enter the height of the Triangle: "))
+    base = float(input("Enter the length of the base of the Triangle: "))
+
+    area = (height * base) / 2
+    print("Area of Triangle: ", area)
+
+
+# Function to compute the volume of Cylinder
+def volumeOfCylinder():
+    radius = float(input("Enter the radius of the Cylinder: "))
+    height = float(input("Enter the height of the Cylinder: "))
+
+    volume = pi * height * radius ** 2
+    print("Volume of Cylinder: ", volume)
+
+
+# Function to compute the volume of Cuboid
+def volumeOfCuboid():
+    length = float(input("Enter the length of the Cuboid: "))
+    breadth = float(input("Enter the breadth of the Cuboid: "))
+    height = float(input("Enter the height of the Cuboid: "))
+
+    volume = length * breadth * height
+    print("Volume of Cuboid: ", volume)
+
+
+# Function to compute the volume of Cube
+def volumeOfCube():
+    edge = float(input("Enter the length of one edge of Cube: "))
+
+    volume = edge ** 3
+    print("Volume of Cube: ", volume)
+
+
+# Function to compute the volume of the Cone
+def volumeOfCone():
+    radius = float(input("Enter the radius of the Cone: "))
+    height = float(input("Enter the height of the Cone: "))
+
+    volume = (pi * height * radius ** 2) / 3
+    print("Volume of Cone: ", volume)
+
+
+# Function to compute the volume of the Sphere
+def volumeOfSphere():
+    radius = float(input("Enter the radius of the Sphere: "))
+
+    volume = (4 * pi * radius ** 3) / 3
+    print("Volume of Sphere: ", volume)
+
+
+# Function to compute the volume of the Hemisphere
+def volumeOfHemisphere():
+    radius = float(input("Enter the radius of the Hemisphere: "))
+
+    volume = (2 * pi * radius ** 3) / 3
+    print("Volume of Hemisphere: ", volume)
+
+
+print("\n*** OPERATIONS ***")
+print("1. Compute Perimeter")
+print("2. Compute Area")
+print("3. Compute Volume")
+
+choice = int(input("Enter your choice: "))
+
+# To compute Perimeter
+if choice == 1:
+    print("*** Select Shape ***")
+    print("1. Circle")
+    print("2. Rectangle")
+    print("3. Rhombus")
+    print("4. Square")
+    print("5. Triangle")
+
+    choice_1 = int(input("Enter your choice: "))
+
+    if choice_1 == 1:
+        perimeterOfCircle()
+
+    elif choice_1 == 2:
+        perimeterOfRectangle()
+
+    elif choice_1 == 3:
+        perimeterOfRhombus()
+
+    elif choice_1 == 4:
+        perimeterOfSquare()
+
+    elif choice_1 == 5:
+        perimeterOfTriangle()
+
+    else:
+        print("!! Enter Right Choice !!")
+
+# To compute Area
+elif choice == 2:
+    print("*** Select Shape ***")
+    print("1. Circle")
+    print("2. Rectangle")
+    print("3. Rhombus")
+    print("4. Square")
+    print("5. Triangle")
+
+    choice_2 = int(input("Enter your choice: "))
+
+    if choice_2 == 1:
+        areaOfCircle()
+
+    elif choice_2 == 2:
+        areaOfRectangle()
+
+    elif choice_2 == 3:
+        areaOfRhombus()
+
+    elif choice_2 == 4:
+        areaOfSquare()
+
+    elif choice_2 == 5:
+        areaOfTriangle()
+
+    else:
+        print("!! Enter Right Choice !!")
+
+# To compute Volume
+elif choice == 3:
+    print("*** Select Shape ***")
+    print("1. Cylinder")
+    print("2. Cuboid")
+    print("3. Cube")
+    print("4. Cone")
+    print("5. Sphere")
+    print("6. Hemisphere")
+
+    choice_3 = int(input("Enter your choice: "))
+
+    if choice_3 == 1:
+        volumeOfCylinder()
+
+    elif choice_3 == 2:
+        volumeOfCuboid()
+
+    elif choice_3 == 3:
+        volumeOfCube()
+
+    elif choice_3 == 4:
+        volumeOfCone()
+
+    elif choice_3 == 5:
+        volumeOfSphere()
+
+    elif choice_3 == 6:
+        volumeOfHemisphere()
+
+    else:
+        print("!! Enter Right Choice !!")
+
+else:
+    print("!! Enter Right Choice !!")
