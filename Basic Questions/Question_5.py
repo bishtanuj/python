73d26cc13d8c3e2a1bc66120bc8f18e2c3e55110
ANUJ BISHT <anujbisht09@gmail.com>
2022-05-22 11:17:50 +0530

    Create Question_5.py

---
+++ Basic Questions/Question_5.py
@@ -0,0 +1,9 @@
+# Write a program to determine the smallest of two entered number
+
+num_1 = int(input("Enter first number: "))
+num_2 = int(input("Enter second number: "))
+
+if num_1 < num_2:
+    print(num_1, "is a smallest number")
+else:
+    print(num_2, "is a smallest number")
