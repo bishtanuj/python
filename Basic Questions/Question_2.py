175d078b095e96aafdd92904eac2e9a420f5276d
ANUJ BISHT <anujbisht09@gmail.com>
2022-05-22 11:12:03 +0530

    Create Question_2.py

---
+++ Basic Questions/Question_2.py
@@ -0,0 +1,23 @@
+# Write a program to demonstrate all arithmetic operations on numbers in python
+
+# Scanning two numbers from user
+num_1 = int(input("Enter first number: "))
+num_2 = int(input("Enter second number: "))
+
+# Applying arithmetic operations on numbers
+add = num_1 + num_2
+subtract = num_1 - num_2
+product = num_1 * num_2
+division = num_1 / num_2
+floor_division = num_1 // num_2
+exponent = num_1 ** num_2
+modulus = num_1 % num_2
+
+# Printing the results of the above arithmetic operations
+print("Addition: ", add)
+print("Subtraction: ", subtract)
+print("Product: ", product)
+print("Division: ", division)
+print("Floor Division: ", floor_division)
+print("Exponent: ", exponent)
+print("Modulus: ", modulus)
