71e067228b7708b2769e1e675b175f1cfad92411
ANUJ BISHT <anujbisht09@gmail.com>
2022-05-22 11:09:28 +0530

    Create Question_1.py

---
+++ Basic Questions/Question_1.py
@@ -0,0 +1,20 @@
+# Write a program to demonstrate different data types in Python
+
+a = 5
+b = "ANUJ"
+c = 5.0
+d = [1, 2, 3, 'a', 'b', 'c']
+e = {1, 2, 3, 4}
+f = (1, 2, 3, 4)
+g = {1: "Delhi", 2: "India"}
+h = True
+
+# Printing the data types of the above declared variables
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
