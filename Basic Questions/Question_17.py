ba0ef441538dcfdb52ccdeeebf981f40bb0427e7
ANUJ BISHT <anujbisht09@gmail.com>
2022-05-27 17:45:14 +0530

    Create Question_17.py

---
+++ Basic Questions/Question_17.py
@@ -0,0 +1,14 @@
+# Write a program to print first 4 even numbers using continue statement
+
+print("First 4 even numbers: ")
+
+for i in range(8):
+    if i % 2 == 0:
+        print(i, end=" ")
+        continue
+
+        
+
+# OUTPUT
+# First 4 even numbers: 
+# 0 2 4 6 
