6c61769891f7f3394041f157d93a9c762d097456
ANUJ BISHT <anujbisht09@gmail.com>
2022-06-02 19:36:28 +0530

    Create modulus.py

---
+++ Operators/modulus.py
@@ -0,0 +1,14 @@
+# Program to show the operation of modulus operator
+
+# Scanning two numbers
+num_1 = int(input("Enter First Number: "))
+num_2 = int(input("Enter Second Number: "))
+
+# Printing modulus
+print("Floor Division:", num_1 % num_2)
+
+
+# OUTPUT
+# Enter First Number: 12
+# Enter Second Number: 8
+# Floor Division: 4
