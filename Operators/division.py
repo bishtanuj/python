fb8a4ed52a45962be09d25779b6fe0d071ebbb11
ANUJ BISHT <anujbisht09@gmail.com>
2022-06-02 19:25:36 +0530

    Create division.py

---
+++ Operators/division.py
@@ -0,0 +1,14 @@
+# Program to show the operation of division operator
+
+# Scanning two numbers
+num_1 = int(input("Enter First Number: "))
+num_2 = int(input("Enter Second Number: "))
+
+# Printing division
+print("Division:", num_1 / num_2)
+
+
+# OUTPUT
+# Enter First Number: 6
+# Enter Second Number: 3
+# Division: 2.0
