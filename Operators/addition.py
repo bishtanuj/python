50662a7d71d8c16e0b7659c7ba8e51be3ec6bc32
ANUJ BISHT <anujbisht09@gmail.com>
2022-06-02 19:03:49 +0530

    Update addition.py

--- Operators/addition.py
+++ Operators/addition.py
@@ -6,3 +6,9 @@ num_2 = int(input("Enter Second Number: "))
 
 # Printing Addition
 print("Addition:", num_1 + num_2)
+
+
+# OUTPUT
+# Enter First Number: 36
+# Enter Second Number: 24
+# Addition: 60
