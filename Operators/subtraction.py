76c08e0dc7cd20c578a8b6e1534c3ba145aeef39
ANUJ BISHT <anujbisht09@gmail.com>
2022-06-02 19:06:48 +0530

    Create subtraction.py

---
+++ Operators/subtraction.py
@@ -0,0 +1,14 @@
+# Program to show the operation of subtraction operator
+
+# Scanning two numbers
+num_1 = int(input("Enter First Number: "))
+num_2 = int(input("Enter Second Number: "))
+
+# Printing Subtraction
+print("Subtraction:", num_1 - num_2)
+
+
+# OUTPUT
+# Enter First Number: 60
+# Enter Second Number: 50
+# Subtraction: 10
