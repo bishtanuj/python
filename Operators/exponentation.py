7725ce15711c72560ca1af4fb274ec1e08575f2e
ANUJ BISHT <anujbisht09@gmail.com>
2022-06-02 19:39:50 +0530

    Create exponentation.py

---
+++ Operators/exponentation.py
@@ -0,0 +1,14 @@
+# Program to show the operation of exponentiation operator
+
+# Scanning two numbers
+num_1 = int(input("Enter First Number: "))
+num_2 = int(input("Enter Second Number: "))
+
+# Printing exponent
+print("Floor Division:", num_1 ** num_2)
+
+
+# OUTPUT
+# Enter First Number: 3
+# Enter Second Number: 2
+# Floor Division: 9
