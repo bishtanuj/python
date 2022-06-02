6681ff0342b26929ea0e01a73110c57687ea09a5
ANUJ BISHT <anujbisht09@gmail.com>
2022-06-02 19:15:29 +0530

    Create multiplication.py

---
+++ Operators/multiplication.py
@@ -0,0 +1,13 @@
+# Program to show the operation of multiplication operator
+
+# Scanning two numbers
+num_1 = int(input("Enter First Number: "))
+num_2 = int(input("Enter Second Number: "))
+
+# Printing multiplication
+print("Multiplication:", num_1 * num_2)
+
+# OUTPUT
+# Enter First Number: 2
+# Enter Second Number: 4
+# Multiplication: 8
