afbc4027ab8141afdecd0f68314da8e3814a8f42
ANUJ BISHT <anujbisht09@gmail.com>
2022-06-02 19:32:53 +0530

    Create floor_division.py

---
+++ Operators/floor_division.py
@@ -0,0 +1,14 @@
+# Program to show the operation of floor division operator
+
+# Scanning two numbers
+num_1 = int(input("Enter First Number: "))
+num_2 = int(input("Enter Second Number: "))
+
+# Printing floor division
+print("Floor Division:", num_1 // num_2)
+
+
+# OUTPUT
+# Enter First Number: 32
+# Enter Second Number: 8
+# Floor Division: 4
