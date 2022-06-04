58f6e57db3190e7d96c2b4348ceab1d2f34d4a70
ANUJ BISHT <anujbisht09@gmail.com>
2022-06-04 16:19:24 +0530

    Create comparison_op_2.py

---
+++ Operators/Programs/comparison_op_2.py
@@ -0,0 +1,20 @@
+# Write a program to show the implementation of '!=' operator
+
+# Scanning two numbers
+num_1 = int(input("Enter first number: "))
+num_2 = int(input("Enter second number: "))
+
+# Printing the result
+print("Conclusion:", num_1 != num_2)
+
+
+OUTPUT
+Enter first number: 3
+Enter second number: 6
+Conclusion: True
+  
+
+OUTPUT
+Enter first number: 6
+Enter second number: 6
+Conclusion: False
