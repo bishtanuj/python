007d8c1416d76031887b9b1762264f163325b81a
ANUJ BISHT <anujbisht09@gmail.com>
2022-06-05 11:22:18 +0530

    Create logical_op_1.py

---
+++ Operators/Programs/logical_op_1.py
@@ -0,0 +1,17 @@
+# Write a program to show the implementation of 'and' operator
+
+# Scanning the number
+num = int(input("Enter a number: "))
+
+# Printing the result
+print("Conclusion:",num < 5 and num < 10)
+
+
+OUTPUT
+Enter a number: 6
+Conclusion: False
+  
+
+OUTPUT
+Enter a number: 3
+Conclusion: True
