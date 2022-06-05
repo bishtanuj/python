ae62eca3389249f076643bdbe506cdb875d28773
ANUJ BISHT <anujbisht09@gmail.com>
2022-06-05 11:27:17 +0530

    Create logical_op_3.py

---
+++ Operators/Programs/logical_op_3.py
@@ -0,0 +1,17 @@
+# Write a program to show the implementation of 'not' operator
+
+# Scanning the number
+num = int(input("Enter a number: "))
+
+# Printing the result
+print("Conclusion:",not(num < 5 and num < 10))
+
+
+# OUTPUT
+# Enter a number: 3
+# Conclusion: False
+  
+  
+# OUTPUT
+# Enter a number: 3
+# Conclusion: False
