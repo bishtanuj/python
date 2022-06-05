ba8d2ac73db878be6e821a8675ddb9ddd46d6d28
ANUJ BISHT <anujbisht09@gmail.com>
2022-06-05 11:25:21 +0530

    Create logical_op_2.py

---
+++ Operators/Programs/logical_op_2.py
@@ -0,0 +1,22 @@
+# Write a program to show the implementation of 'or' operator
+
+# Scanning the number
+num = int(input("Enter a number: "))
+
+# Printing the result
+print("Conclusion:",num < 5 or num == 5)
+
+
+OUTPUT
+Enter a number: 3
+Conclusion: True
+
+  
+OUTPUT
+Enter a number: 5
+Conclusion: True
+
+  
+OUTPUT
+Enter a number: 6
+Conclusion: False
