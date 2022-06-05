b11553defc0b50d5c56d9e0012ff26fadd20804e
ANUJ BISHT <anujbisht09@gmail.com>
2022-06-05 11:27:33 +0530

    Update logical_op_1.py

--- Operators/Programs/logical_op_1.py
+++ Operators/Programs/logical_op_1.py
@@ -7,11 +7,11 @@ num = int(input("Enter a number: "))
 print("Conclusion:",num < 5 and num < 10)
 
 
-OUTPUT
-Enter a number: 6
-Conclusion: False
+# OUTPUT
+# Enter a number: 6
+# Conclusion: False
   
 
-OUTPUT
-Enter a number: 3
-Conclusion: True
+# OUTPUT
+# Enter a number: 3
+# Conclusion: True
