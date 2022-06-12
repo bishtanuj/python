3dc3dab724242aec77e9948fa796294b7231a8e5
ANUJ BISHT <anujbisht09@gmail.com>
2022-06-12 15:04:57 +0530

    Update Question_4.py

--- Basic Questions/Questions Folder/Question_4.py
+++ Basic Questions/Questions Folder/Question_4.py
@@ -6,3 +6,12 @@ if num % 2 == 0:
     print(num, "is an even number")
 else:
     print(num, "is an odd number")
+
+    
+# OUTPUT
+# Enter the number: 369
+# 369 is an odd number
+
+# OUTPUT
+# Enter the number: 562
+# 562 is an even number
