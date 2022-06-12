141ad4c21006d619d24244fa0aac59255415da22
ANUJ BISHT <anujbisht09@gmail.com>
2022-06-12 15:03:48 +0530

    Update Question_3.py

--- Basic Questions/Questions Folder/Question_3.py
+++ Basic Questions/Questions Folder/Question_3.py
@@ -6,3 +6,12 @@ if year % 4 == 0 or year % 400 == 0 and year % 100 != 0:
     print(year, "is a leap year")
 else:
     print(year, "is not a leap year")
+
+
+# OUTPUT  
+# Enter the year: 1998
+# 1998 is not a leap year
+
+# OUTPUT
+# Enter the year: 2008
+# 2008 is a leap year
