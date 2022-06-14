bbb0510d2880de3b14963c526f722a60295e8d92
ANUJ BISHT <anujbisht09@gmail.com>
2022-06-14 07:39:19 +0530

    Create membership_op_2.py

---
+++ Operators/Programs/membership_op_2.py
@@ -0,0 +1,16 @@
+# Program to implement 'not in' operator
+
+list_1 = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+
+var = input("Enter an alphabet: ")
+
+print("Conclusion:", var not in list_1)
+
+# OUTPUT
+# Enter an alphabet: a
+# Conclusion: False
+
+  
+# OUTPUT
+# Enter an alphabet: j
+# Conclusion: True
