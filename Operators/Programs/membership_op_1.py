4bc0a21e441a282de63a4bc3cb25f0d08f1f1d7f
ANUJ BISHT <anujbisht09@gmail.com>
2022-06-14 07:38:11 +0530

    Create membership_op_1.py

---
+++ Operators/Programs/membership_op_1.py
@@ -0,0 +1,16 @@
+# Program to implement 'in' operator
+
+list_1 = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+
+var = input("Enter an alphabet: ")
+
+print("Conclusion:", var in list_1)
+
+
+# OUTPUT
+# Enter an alphabet: b
+# Conclusion: False
+  
+# OUTPUT
+# Enter an alphabet: E
+# Conclusion: True
