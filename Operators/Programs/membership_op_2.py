a12d4de137e883fbdcad428968d2d34e84b5b97e
ANUJ BISHT <anujbisht09@gmail.com>
2022-06-14 07:23:59 +0530

    Create membership_op_2.py

---
+++ Operators/Programs/membership_op_2.py
@@ -0,0 +1,16 @@
+# Program to implement the 'is not' operator
+
+num_1 = int(input("Enter first number: "))
+num_2 = int(input("Enter second number: "))
+
+print("Conclusion:", num_1 is not num_2)
+
+# OUTPUT
+# Enter first number: 3
+# Enter second number: 6
+# Conclusion: True
+  
+# OUTPUT
+# Enter first number: 3
+# Enter second number: 3
+# Conclusion: False
