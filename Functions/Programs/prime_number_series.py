76c0e6990813b503455e05fb9931d68df050528c
ANUJ BISHT <anujbisht09@gmail.com>
2022-08-16 21:54:16 +0530

    Update prime_number_series.py

--- Functions/Programs/prime_number_series.py
+++ Functions/Programs/prime_number_series.py
@@ -1,27 +1,32 @@
 # Write a program to print the series of prime numbers in the given range by user using user defined function
 
-def prime(num_1, num_2):
-    for number in range(num_1, num_2 + 1):
+# Function to print the range of prime numbers
+def prime_number(lower, upper):
+    for number in range(lower, upper + 1):
         if number > 1:
             for i in range(2, number):
-                if (number % i) == 0:
+                if number % i == 0:
                     break
             else:
                 print(number, end=" ")
 
 
-num_1 = int(input("Enter the minimum limit of the prime number series: "))
-num_2 = int(input("Enter the maximum limit of the prime number series: "))
-print("The prime numbers within range", num_1, "and", num_2, "is/are:")
-if num_1 <= 1:
-    num_1 = 2
-if num_2 <= 1:
-    num_2 = 2
-prime(num_1, num_2)
+if __name__ == "__main__":
+    # scanning the limits of the range from the user
+    lower_limit = int(input("Enter the lower limit: "))
+    upper_limit = int(input("Enter the upper limit: "))
 
+    if lower_limit <= 0 or upper_limit <= 0:
+        print("Enter positive number as a limit")
+    else:
+        # printing the required range of the prime numbers
+        print(f"Prime numbers within range of {lower_limit} &{upper_limit}:")
+        prime_number(lower_limit, upper_limit)
 
+        
 # OUTPUT
-# Enter the minimum limit of the prime number series: 1
-# Enter the maximum limit of the prime number series: 10
-# The prime numbers within range 1 and 10 is/are:
-# 2 3 5 7 
+# Enter the lower limit: 10
+# Enter the upper limit: 20
+# Prime numbers within range of 10 &20:
+# 11 13 17 19 
+
