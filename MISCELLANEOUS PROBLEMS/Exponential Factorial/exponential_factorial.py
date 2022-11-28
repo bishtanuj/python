8cac47804417a46d574d7e28f0987fec19a141a4
ANUJ BISHT <anujbisht09@gmail.com>
2022-11-28 19:19:45 +0530

    Update exponential_factorial.py

--- MISCELLANEOUS PROBLEMS/Exponential Factorial/exponential_factorial.py
+++ MISCELLANEOUS PROBLEMS/Exponential Factorial/exponential_factorial.py
@@ -4,15 +4,17 @@
 def exponential_factorial(num):
     if num == 0 or num == 1:
         return 1
-    elif num > 1:
+    elif num > 1 and num < 5:
         return pow(num, pow(num-1, num-2))
+    elif num > 4:
+        return ("Infinity")
     else:
         return ("Enter Positive Value")
 
 
 # Driver Function
 if __name__ == '__main__':
-    num = int(input("Enter the number: "))
+    num = int(input("Enter the positive number less than or equals to 4: "))
     if num >= 0: 
         print(f"Exponential Factorial of {num} = {exponential_factorial(num)}")
     else:
