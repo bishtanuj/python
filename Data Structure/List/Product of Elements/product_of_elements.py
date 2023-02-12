e17c12734636f3ead415cd48b1f2c917983a7965
ANUJ BISHT <anujbisht09@gmail.com>
2023-02-12 09:20:10 +0530

    Update product_of_elements.py

--- Data Structure/List/Product of Elements/product_of_elements.py
+++ Data Structure/List/Product of Elements/product_of_elements.py
@@ -1,2 +1,26 @@
 # Program to find the product of elements of list
 
+# Creating an empty list
+list = []
+
+# Scanning size of the list
+size = int(input("Enter the size of the list: "))
+
+# Scanning the list
+for i in range(size):
+    print(f"Element [{i}]: ", end = "")
+    element = int(input())
+
+    list.append(element)
+
+# Printing the list
+print ("List:")
+print (list)
+
+product = 1
+# Performing Addition of elements of list
+for element in range(0, size):
+    product = product * list[element]
+
+# Printing addition of elements of list
+print("Product:", product)
