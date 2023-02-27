7941d297492abaf827ec8f255cd92b71e835397d
ANUJ BISHT <anujbisht09@gmail.com>
2023-02-27 18:52:46 +0530

    Update cons_vowel_words.py

--- MISCELLANEOUS PROBLEMS/Play With Strings/cons_vowel_words.py
+++ MISCELLANEOUS PROBLEMS/Play With Strings/cons_vowel_words.py
@@ -1,1 +1,19 @@
+# Program to count the number of words, consonants and vowels in a string
+
+string = input("Enter the string: ")
+length_string = len(string)
+count_vowel = 0
+count_cons = 0
+count_words = 0
+for i in range(length_string):
+    if string[i] in ['a','e','i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']:
+        count_vowel += 1
+    elif string[i] == " ":
+        count_words += 1
+    else:
+        count_cons += 1
+
+print(f"Total number of words: {count_words + 1}")
+print(f"Total number of vowel: {count_vowel}")
+print(f"Total number of consonants: {count_cons}")
 
