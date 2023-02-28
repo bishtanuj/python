2a56d140f8dcf43e02fde58ff68892564e445988
ANUJ BISHT <anujbisht09@gmail.com>
2023-02-28 20:31:26 +0530

    Update palindrome_number.py

--- MISCELLANEOUS PROBLEMS/Play With Strings/palindrome_number.py
+++ MISCELLANEOUS PROBLEMS/Play With Strings/palindrome_number.py
@@ -1,1 +1,13 @@
+# Program to check whether the given number is palindrome or not
 
+number = int(input("Enter a number: "))
+reverse = 0
+temp = number
+while temp > 0:
+    temp_1 = temp % 10
+    reverse = reverse*10 + temp_1
+    temp //= 10
+if number == reverse:
+    print(f"{number} is a palindrome number")
+else:
+    print(f"{number} is not a palindrome number")
