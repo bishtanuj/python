d261f71794a097bd8113ceff49a8e5b2946d0ba9
ANUJ BISHT <anujbisht09@gmail.com>
2023-03-03 22:52:07 +0530

    Update shuffle_element_divisibility.py

--- MISCELLANEOUS PROBLEMS/Play With Strings/shuffle_element_divisibility.py
+++ MISCELLANEOUS PROBLEMS/Play With Strings/shuffle_element_divisibility.py
@@ -1,1 +1,14 @@
+# Program to check whether the integer is divisible by 5 or not after shuffling its digits
+
+number = int(input("Enter a number: "))
+count = 0
+while number > 0:
+    temp = number % 10
+    if temp % 5 == 0:
+        count += 1
+    number //= 10
+if count == 0:
+    print("No")
+elif count > 0:
+    print("Yes")
 
