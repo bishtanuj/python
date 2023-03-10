6a879a3c1369df5e909615c28fc5a7ad77655de8
ANUJ BISHT <anujbisht09@gmail.com>
2023-03-10 09:47:12 +0530

    Update reverse_string.py

--- MISCELLANEOUS PROBLEMS/Play With Strings/reverse_string.py
+++ MISCELLANEOUS PROBLEMS/Play With Strings/reverse_string.py
@@ -1,1 +1,25 @@
+# Write a program to reverse a string
 
+# Test case
+# Input: My.name.is.Anuj
+# Output: Anuj.is.name.My
+
+# Scan a string
+string = input("Enter a string: ")
+
+# Split the string form "."
+split_string_list = string.split(".")
+
+# Revesre the split_string_list
+reverse_list = split_string_list[::-1]
+
+# Printing the result
+count = 0
+print("Result: ", end="")
+for i in range(len(reverse_list) - 1):
+    print(reverse_list[i]+".",end="")
+    count = i+1
+print(reverse_list[count])
+
+
+# Time complexity: O(n)
