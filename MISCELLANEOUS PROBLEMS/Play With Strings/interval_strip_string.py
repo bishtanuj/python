2bb5424ce676057794386bc4584ec92d1ea99d7a
ANUJ BISHT <anujbisht09@gmail.com>
2023-03-30 08:39:05 +0530

    Update interval_strip_string.py

--- MISCELLANEOUS PROBLEMS/Play With Strings/interval_strip_string.py
+++ MISCELLANEOUS PROBLEMS/Play With Strings/interval_strip_string.py
@@ -1,1 +1,19 @@
+# Program to print the string after stripping the string at continuous intervals
+
+# Scan the length of string
+length_of_string = int(input("Enter the length of string: "))
+
+# Scan the string 
+string = input(f"Enter the string of length {length_of_string}: ")[:length_of_string]
+
+# Print the result
+print(string[0], end="")
+i = 1
+while i < length_of_string:
+    if i % 4 != 0:
+        print(string[i], end="")
+    else:
+        print()
+        print(string[i], end="")
+    i += 1
 
