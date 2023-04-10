340b4204d78609fb0b64e32b74b955ed5cfa4329
ANUJ BISHT <anujbisht09@gmail.com>
2023-04-10 07:49:45 +0530

    Update palindrome_number.py

--- MISCELLANEOUS PROBLEMS/Play With Strings/palindrome_number.py
+++ MISCELLANEOUS PROBLEMS/Play With Strings/palindrome_number.py
@@ -1,5 +1,6 @@
 # Program to check whether the given number is palindrome or not
 
+# METHOD 1:
 number = int(input("Enter a number: "))
 reverse = 0
 temp = number
@@ -11,3 +12,26 @@ if number == reverse:
     print(f"{number} is a palindrome number")
 else:
     print(f"{number} is not a palindrome number")
+
+    
+    
+# METHOD 2
+# Scan a number
+number = int(input("Enter the number: "))
+
+# Check whether the number is positive or not
+# Because negative number cannot be a palindrome
+if number < 0:
+    print(f"{number} is not a palindrome")
+else:
+    # Convert the number into list
+    list_number = [int(i) for i in str(number)]
+
+    # Reverse the list
+    reverse_list_number = list_number[::-1]
+
+    # Compare both lists
+    if list_number == reverse_list_number:
+        print(f"{number} is a palindrome")
+    else:
+        print(f"{number} is not a palindrome")
