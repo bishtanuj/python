dd31bbe1769408621b251ef29a219acf0208ac2c
ANUJ BISHT <anujbisht09@gmail.com>
2023-05-20 10:37:53 +0530

    Update palindrome_two_pointers.py

--- MISCELLANEOUS PROBLEMS/Play With Strings/palindrome_two_pointers.py
+++ MISCELLANEOUS PROBLEMS/Play With Strings/palindrome_two_pointers.py
@@ -1,1 +1,28 @@
+# Program to check whether the input string is palindrome
+
+# function to check the palindrome
+def check_if_palindrome(a: str) -> bool:
+    # setting of two pointers
+    i, j = 0, len(a) - 1
+    while i < j:
+        if a[i] != a[j]:
+            return False
+        else:
+            i += 1
+            j -= 1
+    return True
+
+
+if __name__ == "__main__":
+    # scanning the string from user
+    string = input("Enter the string: ")
+
+    # Calling function to check the palindrome
+    result = check_if_palindrome(string)
+
+    # printing the conclusion
+    if result:
+        print("It is a palindrome")
+    else:
+        print("It is not a palindrome")
 
