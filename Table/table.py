561dbab81a58a3835c8537733c7d2da678b483fa
ANUJ BISHT <anujbisht09@gmail.com>
2023-06-10 22:44:59 +0530

    Update table.py

--- Table/table.py
+++ Table/table.py
@@ -8,3 +8,18 @@ print(f"Table of {number}: ")
 for i in range(1,11):
     product = number * i
     print(f"{number} * {i} = {product}")
+
+    
+
+Enter the number: 5
+Table of 5: 
+5 * 1 = 5
+5 * 2 = 10
+5 * 3 = 15
+5 * 4 = 20
+5 * 5 = 25
+5 * 6 = 30
+5 * 7 = 35
+5 * 8 = 40
+5 * 9 = 45
+5 * 10 = 50
