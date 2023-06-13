93b8707f765eaeebf05aec66edb45f633c34aba1
ANUJ BISHT <anujbisht09@gmail.com>
2023-06-13 17:15:49 +0530

    Create reverse_string_1.py

---
+++ MISCELLANEOUS PROBLEMS/Play With Strings/reverse_string_1.py
@@ -0,0 +1,31 @@
+# Program to print the reverse of string
+
+# function to reverse the string
+def reverse_string(nums: list[str]) -> str:
+
+    # let's create two pointers
+    i, j = 0, len(nums) - 1
+
+    # swap the elements to reverse the list
+    while i <= j:
+        nums[i], nums[j] = nums[j], nums[i]
+        i += 1
+        j -= 1
+
+    # convert the list into string and return as an answer
+    string_new = ""
+    for element in nums:
+        string_new += element
+    return string_new
+
+
+# driver function
+if __name__ == '__main__':
+    # scan the string
+    string = input("Enter the string: ")
+
+    # convert the string into list
+    string_list = [i for i in string]
+
+    # print the reverse string
+    print(f"Reverse String: {reverse_string(string_list)}")
