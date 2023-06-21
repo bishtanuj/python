625dd1f28ecb04321865874cd289503cb8d3b86c
ANUJ BISHT <anujbisht09@gmail.com>
2023-06-21 15:36:54 +0530

    Update question_6.py

--- QUESTIONS/Basic/question_6.py
+++ QUESTIONS/Basic/question_6.py
@@ -1,5 +1,5 @@
 """ 
-Write a Python program which accepts a sequence of comma-separated numbers from user and generate a list and a tuple with those numbers. Go to the editor
+Write a Python program which accepts a sequence of comma-separated numbers from user and generate a list and a tuple with those numbers.
 Sample data : 3, 5, 7, 23
 Output :
 List : ['3', ' 5', ' 7', ' 23']
