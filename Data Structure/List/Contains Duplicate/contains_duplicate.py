996aeb7a3be677f3d515e43a31b34ed915252c35
ANUJ BISHT <anujbisht09@gmail.com>
2023-07-01 11:18:52 +0530

    Update contains_duplicate.py

--- Data Structure/List/Contains Duplicate/contains_duplicate.py
+++ Data Structure/List/Contains Duplicate/contains_duplicate.py
@@ -8,3 +8,11 @@ def contains_duplicate(nums: list[int]) -> bool:
       break
   return False
 
+
+if __name__ == "__main__":
+  numbers = list(map(int, input("Enter the number seperated by space: ").split()))
+  result = contains_duplicate(numbers)
+  if result:
+    print("List contains duplicate elements")
+  else:
+    print("List doesnot contains duplicate elements")
