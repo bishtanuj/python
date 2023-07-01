9662ed913413f2b35ddda1e03be8f25e28b24962
ANUJ BISHT <anujbisht09@gmail.com>
2023-07-01 11:09:52 +0530

    Create oddEven_1.py

---
+++ Checking Odd - Even/oddEven_1.py
@@ -0,0 +1,18 @@
+# Program to find whether the number is odd or even
+
+# funtion to check a number
+def check_odd_even(num: int) -> bool:
+  return True if num % 2 == 0 else False
+
+
+# driver function
+if __name__ == "__main__":
+  # Scan the number from user
+  number = int(input("Enter the number: "))
+
+  # call function to check the number and saves the output in "result" variable
+  result = check_odd_even(number)
+  if result:
+    print(f"{number} is an even number")
+  else:
+    print(f"{number} is an odd number")
