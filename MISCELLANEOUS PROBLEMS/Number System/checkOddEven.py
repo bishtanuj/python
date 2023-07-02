c78dfebe1942e4727e23b94dac2a3bf376b2da42
ANUJ BISHT <anujbisht09@gmail.com>
2023-07-02 22:22:19 +0530

    Update checkOddEven.py

--- MISCELLANEOUS PROBLEMS/Number System/checkOddEven.py
+++ MISCELLANEOUS PROBLEMS/Number System/checkOddEven.py
@@ -1,9 +1,11 @@
 # Program to check whether the number is odd or even
 
+# function to check whether the number is odd or even
 def checkOddEven(num: int) -> bool:
   return True if num % 2 == 0 else False
 
 
+# driver function
 if __name__ == "__main__":
   num = int(input("Enter the number: "))
   result = checkOddEven(num)
