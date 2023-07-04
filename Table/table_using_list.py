095fbba990ba89a2750add80d88d71701a3b6316
ANUJ BISHT <anujbisht09@gmail.com>
2023-07-04 22:27:08 +0530

    Update table_using_list.py

--- Table/table_using_list.py
+++ Table/table_using_list.py
@@ -1,5 +1,13 @@
 # Program to print the table of a number using list
 
+def printTable(num: int):
+  table = [i*num for i in range(1, 11)]
+  k = 1
+  for i in table:
+    print(f"{num} * {k} = {i}")
+    k += 1
+
+
 if __name__ == "__main__":
   num = int(input("Enter the number: "))
   printTable(num)
