15ed32be3d63d2c7aa5ba9c0506aef9e1d5b7508
ANUJ BISHT <anujbisht09@gmail.com>
2023-07-06 19:43:26 +0530

    Update single_occurrence.py

--- Data Structure/List/Single Occurrence/single_occurrence.py
+++ Data Structure/List/Single Occurrence/single_occurrence.py
@@ -1,2 +1,9 @@
 # Program to find the single occurred elements in a list
 
+from collections import Counter
+
+def singleOccurrence(num: list[int]) -> list[int]:
+  occurrence = Counter(num)
+
+  K = [i for i in occurrence if occurrence[i] == 1]
+  return K
