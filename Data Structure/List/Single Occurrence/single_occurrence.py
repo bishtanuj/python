8e4fa85d16ead803c83449f30c20c5d0eccd379c
ANUJ BISHT <anujbisht09@gmail.com>
2023-07-06 19:51:54 +0530

    Update single_occurrence.py

--- Data Structure/List/Single Occurrence/single_occurrence.py
+++ Data Structure/List/Single Occurrence/single_occurrence.py
@@ -2,13 +2,17 @@
 
 from collections import Counter
 
+# function to find the elements that occurred only once
 def singleOccurrence(num: list[int]) -> list[int]:
+  # create a dictionary "occurrence" having the elements of a num as Keys and their occurrence as their values respectively
   occurrence = Counter(num)
 
+  # create a list K that contains the elements that occured only once in num
   K = [i for i in occurrence if occurrence[i] == 1]
   return K
 
 
+# driver function
 if __name__ == "__main__":
   numbers = list(map(int, input("Enter the list of a numbers: ").split())
   print(f"Elements that occured once in a list: {singleOccurrence(numbers)}")
