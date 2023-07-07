750f0cfe882c03b9f69a54d71c4964d1a4ab635c
ANUJ BISHT <anujbisht09@gmail.com>
2023-07-07 22:34:56 +0530

    Update contains_duplicate_1.py

--- Data Structure/List/Contains Duplicate/contains_duplicate_1.py
+++ Data Structure/List/Contains Duplicate/contains_duplicate_1.py
@@ -1,2 +1,16 @@
 # Program to find the duplicate elements in list
 
+from collections import Counter
+
+
+def checkDuplicate(nums: list[int]) -> list[int]:
+  occurrence = Counter(nums)
+  K = [i for i in occurrence if occurrence[i] > 1]
+  return K
+
+
+
+if __name__ == "__main__":
+  numbers = list(map(int, input("Enter the numbers: ").split()))
+  print(f"Numbers that occurs more than once in a list: {checkDuplicate(numbers)}")
+  
