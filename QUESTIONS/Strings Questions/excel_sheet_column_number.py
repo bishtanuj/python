6e04b50fe1d73dd7f64d60c0e62b1743a91b2253
ANUJ BISHT <anujbisht09@gmail.com>
2023-07-08 11:12:20 +0530

    Update excel_sheet_column_number.py

--- QUESTIONS/Strings Questions/excel_sheet_column_number.py
+++ QUESTIONS/Strings Questions/excel_sheet_column_number.py
@@ -1,1 +1,11 @@
 # Program to print the excel sheet column number
+
+def titleNumber(column: str) -> int:
+  result = 0
+  numbers = [i for i in range(1, 27)]
+  letters = "ABCDEFGHIJHKLMNOPQRSTUVWXYZ"
+  dictionary = dict(zip(letters, numbers))
+  for i in column:
+    result = result * 26 + dictionary[i]
+  return result
+  
