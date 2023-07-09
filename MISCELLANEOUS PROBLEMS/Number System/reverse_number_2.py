0c9cacbfa88b9fe0fa8fa18cb45bde56e5162798
ANUJ BISHT <anujbisht09@gmail.com>
2023-07-10 01:45:16 +0530

    Update reverse_number_2.py

--- MISCELLANEOUS PROBLEMS/Number System/reverse_number_2.py
+++ MISCELLANEOUS PROBLEMS/Number System/reverse_number_2.py
@@ -1,1 +1,12 @@
 # Program to reverse a number
+
+
+def reverseNumber(num: int) -> int:
+  K = [int(i) i for i in str(num)]
+  reverse_K = K[::-1]
+  str_num = ""
+  for i in reverse_K:
+    str_num += i
+  return int(str_num)
+
+
