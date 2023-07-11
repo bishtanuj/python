44cdff4cdfca478c2310f3b033694c38b2566df8
ANUJ BISHT <anujbisht09@gmail.com>
2023-07-11 17:02:00 +0530

    Update list_intersection.py

--- Data Structure/List/Intersection of two lists/list_intersection.py
+++ Data Structure/List/Intersection of two lists/list_intersection.py
@@ -7,3 +7,9 @@ def intersect(nums1: list[int], nums2: list[int]) -> list[int]:
       K.append(i)
   return set(K)
 
+
+if __name__ == "__main__":
+  nums1 = list(map(int, input("Enter the elements of Array 1: ").split()))
+  nums2 = list(map(int, input("Enter the elements of Array 2: ").split()))
+  print(f"Intersection: {intersect(nums1, nums2)}")
+
