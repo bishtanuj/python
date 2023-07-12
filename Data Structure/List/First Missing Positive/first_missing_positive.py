18017a35d4335d29e62db679a68398fffe9edd4d
ANUJ BISHT <anujbisht09@gmail.com>
2023-07-12 21:52:47 +0530

    Update first_missing_positive.py

--- Data Structure/List/First Missing Positive/first_missing_positive.py
+++ Data Structure/List/First Missing Positive/first_missing_positive.py
@@ -1,1 +1,13 @@
 # Program to find the first missing positive element in a list
+
+
+def firstMissingPositive(nums: list[int]) -> int:
+  K = sorted(set(nums))
+  i = 1
+  for num in K:
+    if num == i:
+      i += 1
+    elif num > 0:
+      return i
+  return i
+
