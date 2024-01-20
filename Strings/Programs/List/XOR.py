46b42a9a87930a844b4083150aa5282325a53409
ANUJ BISHT <anujbisht09@gmail.com>
2024-01-20 11:19:01 +0530

    Create XOR.py

---
+++ Strings/Programs/List/XOR.py
@@ -0,0 +1,16 @@
+# Program to compute the xor of two strings
+
+def compute_xor(str1: str, str2: str) -> str:
+    result = ""
+    for i in range(len(str1)):
+        if str1[i] == str2[i]:
+            result += '0'
+        else:
+            result += '1'
+    return result
+
+
+if __name__ == '__main__':
+    string1 = input("Enter first string: ")
+    string2 = input("Enter second string: ")
+    print(f"XOR of {string1} & {string2}: {compute_xor(string1, string2)}")
