f81d9bf22fecf5d5963048a5ec9ec57a0fe546f2
ANUJ BISHT <anujbisht09@gmail.com>
2024-01-20 11:11:34 +0530

    Update and rename Subarray.py to subarray.py

--- Data Structure/Array/subarray.py
+++ Data Structure/Array/subarray.py
@@ -12,3 +12,10 @@ def compute_subarray(array: list[int]) -> list[list[int]]:
 if __name__ == '__main__':
     arr = list(map(int, input("Enter the elements of the array: ").split(" ")))
     print(f"List of all subarray: {compute_subarray(arr)}")
+
+
+'''
+OUTPUT:
+Enter the elements of the array: 1 8 9 7
+List of all subarray: [[1], [1, 8], [1, 8, 9], [1, 8, 9, 7], [8], [8, 9], [8, 9, 7], [9], [9, 7], [7]]
+'''
