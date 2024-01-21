0d7aaf79811a56092f1d99ca582cb6e3ca247930
ANUJ BISHT <anujbisht09@gmail.com>
2024-01-21 10:47:39 +0530

    Update single_occurrence_2.py

--- Data Structure/List/Single Occurrence/single_occurrence_2.py
+++ Data Structure/List/Single Occurrence/single_occurrence_2.py
@@ -10,11 +10,15 @@ def single_occurrence(num: list[int]) -> list[int]:
       occurrence[element] += 1
     else:
       occurrence[element] = 1
-      
+
+  # create a list 'result' that contains the elements that occurred only once in num
   result = [key for key in occurrence if occurrence[key] == 1]
   return result
 
-
+# driver function
 if __name__ == '__main__':
-  array = list(map(int, input().split()))
-  print(f"{single_occurrence(array)}")
+  # scan the list from the user
+  array = list(map(int, input("Enter the elements of the list: ").split()))
+
+  # Print the conclusion
+  print(f"List of elements that occurred only once in the array: {single_occurrence(array)}")
