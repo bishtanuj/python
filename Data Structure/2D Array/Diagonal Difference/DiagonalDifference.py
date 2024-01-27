899bcee1b419d75307d2e0285d15f0e2def35212
ANUJ BISHT <anujbisht09@gmail.com>
2024-01-27 21:53:25 +0530

    Create DiagonalDifference.py

---
+++ Data Structure/2D Array/Diagonal Difference/DiagonalDifference.py
@@ -0,0 +1,23 @@
+def diagonal_difference(array: list[list[int]], size: int) -> int:
+    diagonal_1, diagonal_2 = 0, 0
+    for i in range(size):
+        for j in range(size):
+            if i == j:
+                diagonal_1 += array[i][j]
+            if i+j == size-1:
+                diagonal_2 += array[i][j]
+
+    difference = diagonal_2 - diagonal_1
+    if difference < 0:
+        return -difference
+    else:
+        return difference
+
+
+if __name__ == '__main__':
+    dimension = int(input("Enter the dimension of square matrix: "))
+    matrix = []
+    print("Enter the elements of square matrix:")
+    for rows in range(dimension):
+        matrix.append(list(map(int, input().rstrip().split())))
+    print(f"Absolute difference of the diagonals of matrix: {diagonal_difference(matrix, dimension)}")
