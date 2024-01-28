b7dd0cfa9b2fd6cba70fb53e44688e844f3b8a35
ANUJ BISHT <anujbisht09@gmail.com>
2024-01-28 19:47:31 +0530

    Create SecondMinimumElement.py

---
+++ Data Structure/Linked List/Programs/List/SecondMinimumElement.py
@@ -0,0 +1,63 @@
+# Program to find the second minimum element
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insertion(self, data):
+        node = Node(data)
+        if self.tail:
+            self.tail.next = node
+            self.tail = node
+        else:
+            self.head = node
+            self.tail = node
+
+    def display(self):
+        current = self.head
+        if self.head:
+            print("Elements of the linked list:", end=" ")
+            while current:
+                print(current.data, end=" ")
+                current = current.next
+        else:
+            print("Linked list is empty!!")
+
+    def compute_second_minimum(self):
+        current = self.head
+        minimum = current.data
+        while current:
+            if current.data < minimum:
+                minimum = current.data
+            else:
+                current = current.next
+        temp = self.head
+        second_minimum = float('infinity')
+        while temp:
+            if minimum < temp.data < second_minimum:
+                second_minimum = temp.data
+            else:
+                temp = temp.next
+        return second_minimum
+
+
+if __name__ == '__main__':
+    linkedlist = LinkedList()
+
+    linkedlist.insertion(10)
+    linkedlist.insertion(11)
+    linkedlist.insertion(12)
+    linkedlist.insertion(15)
+    linkedlist.insertion(18)
+    linkedlist.insertion(20)
+
+    linkedlist.display()
+
+    print(f"\nSecond minimum element of the linked list: {linkedlist.compute_second_minimum()}")
