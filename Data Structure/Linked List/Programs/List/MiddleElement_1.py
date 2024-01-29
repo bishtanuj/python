14df44c9a6a3131c8c61d3c2f44fee97d89192db
ANUJ BISHT <anujbisht09@gmail.com>
2024-01-29 19:11:17 +0530

    Create MiddleElement_1.py

---
+++ Data Structure/Linked List/Programs/List/MiddleElement_1.py
@@ -0,0 +1,70 @@
+# Program to print the middle element of linked list
+
+# class to create a node
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+# class to create a linked list
+class LinkedList:
+    # function to create an empty linked list
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    # function to insert the data into linked list
+    def insertion(self, data):
+        node = Node(data)
+        if self.tail:
+            self.tail.next = node
+            self.tail = node
+        else:
+            self.head = node
+            self.tail = node
+
+    # generator function to display the linked list
+    def display(self):
+        current = self.head
+        while current:
+            value = current.data
+            current = current.next
+            yield value
+
+    # function to compute the middle element of linked list
+    def compute_middle(self):
+        current = self.head
+        count = 0
+        while current:
+            count += 1
+            current = current.next
+        if count % 2 != 0:
+            mid = count // 2
+        else:
+            x = count // 2
+            y = (count // 2) + 1
+            mid = (x + y) // 2
+        temp = self.head
+        count_new = 0
+        while temp:
+            if count_new == mid:
+                print(temp.data)
+                break
+            else:
+                count_new += 1
+                temp = temp.next
+
+
+# driver
+if __name__ == '__main__':
+    items = LinkedList()
+    items.insertion(12)
+    items.insertion(13)
+    items.insertion(14)
+    items.insertion(80)
+    print("Elements of Linked List:", end=" ")
+    for i in items.display():
+        print(i, end=" ")
+    print("\nMiddle Element:", end=" ")
+    items.compute_middle()
