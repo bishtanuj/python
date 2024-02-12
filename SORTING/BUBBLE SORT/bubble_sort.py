dc6db77966eff27efe3c82aca7d9a7cb99b1e55d
ANUJ BISHT <anujbisht09@gmail.com>
2024-02-12 19:56:57 +0530

    Update bubble_sort.py

--- SORTING/BUBBLE SORT/bubble_sort.py
+++ SORTING/BUBBLE SORT/bubble_sort.py
@@ -1,31 +1,16 @@
-# Program to perform bubble sort
+# Program to sort the list using bubble sort technique
 
-# Function to scan the list
-def scan_list(list_1, size):
-    print(f"Enter the {size} elements of the array:")
-    for i in range(size):
-        element = int(input(f"Element [{i}]: "))
-        list_1.append(element)
-
-
-# Function to print the list
-def print_list(list_1):
-    print(list_1)
+# function to perform bubble sort
+def bubble_sort(nums: list[int], size: int):
+    for i in range(size - 1):
+        for j in range(size - 1 - i):
+            if nums[j] > nums[j + 1]:
+                nums[j], nums[j + 1] = nums[j + 1], nums[j]
 
 
-# Function to perform bubble sort
-def bubble_sort(list_1, size):
-    for i in range(size-1):
-        for j in range(size-1-i):
-            if list_1[j] > list_1[j+1]:
-                (list_1[j], list_1[j+1]) = (list_1[j+1], list_1[j])
-
-
-# Driver Function
 if __name__ == '__main__':
-    list_1 = []
-    size = int(input("Enter the number of elements of the list: "))
-    scan_list(list_1, size)
-    print_list(f"List: {list_1}")
-    bubble_sort(list_1, size)
-    print_list(f"Sorted List: {list_1}")
+    N = int(input("Enter the total number of elements: "))
+    array = list(map(int, input(f"Enter the {N} element(s) of the list: ").split()))[:N]
+    print(f"List: {array}")
+    bubble_sort(array, N)
+    print(f"Sorted List: {array}")
