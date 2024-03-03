dcb6fbf138444b151330edd437fc99089d8cb942
ANUJ BISHT <anujbisht09@gmail.com>
2024-03-03 13:20:28 +0530

    Update RemoveFromHead.py

--- Data Structure/Linked List/Programs/List/RemoveFromHead.py
+++ Data Structure/Linked List/Programs/List/RemoveFromHead.py
@@ -53,3 +53,9 @@ if __name__ == '__main__':
 
     linkedlist.delete_from_head()
     linkedlist.display()
+
+"""
+OUTPUT:
+Linked List: 1->2->3->4->5->6->7->8->9->10->11->None
+Linked List: 2->3->4->5->6->7->8->9->10->11->None
+"""
