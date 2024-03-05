7b7cec38f436e59866945184ba9a2eb85d9e5a1c
ANUJ BISHT <anujbisht09@gmail.com>
2024-03-05 21:04:41 +0530

    Create CreateBinarySearchTree.py

---
+++ Data Structure/Binary Search Tree/Programs/List/CreateBinarySearchTree.py
@@ -0,0 +1,9 @@
+# Program to create a binary search tree
+
+# class to create a node
+class TreeNode:
+  def __init__(self, data):
+    self.data = data
+    self.left = None
+    self.right = None
+    
