038b81f0ee2a3b430dfb8ca240a27cc54255cd81
ANUJ BISHT <anujbisht09@gmail.com>
2024-03-28 10:10:21 +0530

    Create CountOddNumbers.py

---
+++ Data Structure/Binary Search Tree/Programs/List/CountOddNumbers.py
@@ -0,0 +1,53 @@
+# Program to count odd numbers in the binary search tree
+
+# class to create a node
+class TreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+
+# class for the binary search tree
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, data):
+        self.root = self.insert_recursive(self.root, data)
+
+    def insert_recursive(self, root, data):
+        if not root:
+            root = TreeNode(data)
+        elif data < root.data:
+            root.left = self.insert_recursive(root.left, data)
+        else:
+            root.right = self.insert_recursive(root.right, data)
+        return root
+
+    def inorder(self, root):
+        if not root:
+            return []
+        else:
+            return self.inorder(root.left) + [root.data] + self.inorder(root.right)
+
+    def count_odd(self):
+        inorder_traversal = self.inorder(self.root)
+        answer = len([i for i in inorder_traversal if i % 2 != 0])
+        return answer
+
+
+if __name__ == '__main__':
+    bst = BinarySearchTree()
+
+    bst.insert(12)
+    bst.insert(24)
+    bst.insert(18)
+    bst.insert(31)
+    bst.insert(36)
+    bst.insert(79)
+    bst.insert(2)
+    bst.insert(1)
+
+    print("Inorder traversal of the binary search tree:", bst.inorder(bst.root))
+    print("Total odd numbers in the binary search tree:", bst.count_odd())
